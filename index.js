b270ecff7a9c7ac57608c7c3a1fda2ac532088fc
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-10-25 17:44:28 +0600

    added the leftside-nav  category

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000  ;
+
+
+app.use(cors())
+const category = require('./data/category.json')
+app.get('/', (req,res) => {
+    res.send('Programming Learn api server running ')
+})
+
+app.get('/category-data', (req,res)=> {
+    res.send(category)
+})
+
+app.listen(port,  ()=> {
+    console.log('Server running on ' , port )
+})
