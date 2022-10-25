cf138f3f793790f33a809040a61c077c80a77fcb
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-10-25 23:51:11 +0600

    added the courses.json file and it is the all courses file

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const port = process.env.PORT || 5000  ;
 
 app.use(cors())
 const category = require('./data/category.json')
+const courses = require ('./data/courses.json')
 app.get('/', (req,res) => {
     res.send('Programming Learn api server running ')
 })
-
+//  load data of category 
 app.get('/category-data', (req,res)=> {
     res.send(category)
+})
+
+// load data of courses details 
+app.get('/courses-data', (req,res) => {
+    res.send(courses);
+    console.log(res.params.id)
 })
 
 app.listen(port,  ()=> {
