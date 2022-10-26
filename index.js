2c539c01cc5710a97b2372b80351f07f372b4b6a
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-10-26 12:14:03 +0600

    added the same category course server data

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000  ;
-
-
 app.use(cors())
+
+
 const category = require('./data/category.json')
 const courses = require ('./data/courses.json')
 app.get('/', (req,res) => {
@@ -18,7 +18,19 @@ app.get('/category-data', (req,res)=> {
 // load data of courses details 
 app.get('/courses-data', (req,res) => {
     res.send(courses);
-    console.log(res.params.id)
+})
+
+// load data by id 
+app.get('/course-details/:id', (req,res) => {
+  const courseId = req.params.id ;
+  const selectedCourse = courses.find(course => course._id === courseId)
+  res.send(selectedCourse);
+})
+// get data same category by category_id 
+app.get('/category/:id', (req,res) => {
+    const reqId = req.params.id ;
+    const cateoryCourse =  courses.filter( course => course.category_id === reqId)
+    res.send(cateoryCourse)
 })
 
 app.listen(port,  ()=> {
